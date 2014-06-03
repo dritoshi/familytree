684fbca3375751cd8a8485088258107b328b5462
Itoshi NIKAIDO <dritoshi@gmail.com>
2014-06-04 00:43:08 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Draw family tree in R/kinship2
+
+## Requirements
+* R
+* kinship2 package
+
+## Usage
+
+    R -q -f familytree.R --args familytree.csv
---
+++ familytree.R
@@ -0,0 +1,32 @@
+library("kinship2")
+
+# get data filename from command option
+args <- commandArgs(TRUE)
+file <- args[1]
+
+# make pdf file name
+pdf.file <- sub(".csv", ".pdf", file)
+
+# load data from data file
+sample.ped <- read.csv(file, header=T)
+
+# make pedigree object
+pedAll <- pedigree(
+  id = sample.ped$id,
+  dadid = sample.ped$father,
+  momid = sample.ped$mother,
+  sex   = sample.ped$sex,
+  famid = sample.ped$ped
+)
+
+# get a family
+ped1basic <- pedAll["1"]
+
+# make id strings
+ids <- paste(sample.ped$id, sample.ped$firstname, sep ="\n")
+
+# plot and save pdf file
+pdf(pdf.file)
+plot(ped1basic, id = ids, status = sample.ped$avail, 
+     affected = sample.ped$affected, cex = 0.5)
+dev.off()
